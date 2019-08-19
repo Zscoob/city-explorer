56428a93427fcf0d3806cde33d60f7994775dbe8
Zscoob <james.zobian@gmail.com>
2019-08-19 16:43:02 -0500

    makesure

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/weather',(request, response) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('server is listening');
 });
